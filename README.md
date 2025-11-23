4d10a26fb5d7b870db3788ab16aee2a01fd8769e
SamiiHC-iespsur <samuel.de.cozar.al@iespoligonosur.org>
2025-11-23 13:34:23 +0100

    Commit inicial

---
+++ README.md
@@ -0,0 +1,2 @@
+# Biblioteca-JDBC
+Proyecto de Acceso de Datos; Sistema de Biblioteca en Java con manipulación de datos de MySql mediante API JDBC.
---
+++ biblioteca/src/main/java/sami/ada/biblio/Main.java
@@ -0,0 +1,7 @@
+package sami.ada.biblio;
+
+public class Main {
+    public static void main(String[] args) {
+        // Implementación pendiente
+    }
+}
